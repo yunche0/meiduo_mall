cd9a592dece1d1f378a6fad48dc1b43431f03fac
cb <1620059368@qq.com>
2026-05-02 14:26:41 +0800

    QQ账号和美多账号绑定

--- meiduo/apps/oauth/urls.py
+++ meiduo/apps/oauth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.oauth.views import QQLoginURLView
+from apps.oauth.views import QQLoginURLView,OAuthQQView
 
 urlpatterns = [
     path('qq/authorization/', QQLoginURLView.as_view()),
+    path('oauth_callback/',OAuthQQView.as_view()),
 ]
